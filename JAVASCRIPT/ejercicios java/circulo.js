7634f7aba7b454ae47e0ccf405a9715557b12ca7
franciscozamora05 <franciscojosezamora05@iesamachado.org>
2024-12-17 14:22:21 +0100

    ejercicios y apuntes js

---
+++ JAVASCRIPT/ejercicios java/circulo.js
@@ -0,0 +1,5 @@
+/* Este fichero contiene el código para ocultar un circulo*/
+function ocultar(id){
+    //Esta función permite ocultar un circulo
+    document.getElementById(id).style.visibility ='hidden';
+}
